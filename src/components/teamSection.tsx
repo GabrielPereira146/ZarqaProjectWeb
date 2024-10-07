5203251fc5915cef69f17cb68d14f51dc58b84a6
Gabriel Pereira <gabriel.brown1112@gmail.com>
2024-10-07 10:47:51 -0300

    feat: component team section

---
+++ src/components/teamSection.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { TeamData } from '../interface/TeamData';
+
+interface TeamSectionProps {
+  title: string;
+  role: string;
+  data: TeamData[];
+  className?: string;
+}
+
+export const TeamSection: React.FC<TeamSectionProps> = ({ title, role, data, className }) => {
+  return (
+    <div className={`flex flex-col pl-16 py-8 gap-4 ${className}`}>
+      <p className="font-extrabold text-4xl text-zinc-800 dark:text-white">{title}</p>
+      <div className="flex flex-row w-full px-6 py-2 gap-8">
+        {data.length > 0 ? (
+          data
+            .filter((teamMember) => teamMember.role.toLowerCase() === role.toLowerCase())
+            .map((teamMember) => (
+              <div key={teamMember.name} className="h-80 w-60 flex flex-col rounded-2xl bg-sand-100/50 dark:bg-sand-200">
+                <img className="w-full h-3/4 rounded-t-2xl" src={teamMember.picture} alt={teamMember.name} />
+                <div className="flex flex-col justify-center align-middle py-4">
+                  <h3 className="text-lg font-semibold text-center">{teamMember.name}</h3>
+                  <p className="text-base font-medium text-black/50 text-center">{teamMember.institution}</p>
+                </div>
+              </div>
+            ))
+        ) : (
+          <p className="text-zinc-500">Carregando...</p> // Mensagem de carregamento
+        )}
+      </div>
+    </div>
+  );
+};
--- src/routes/team.tsx
+++ src/routes/team.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { TeamData } from '../interface/TeamData'; // Importar a interface, se necessário
+import { TeamSection } from "../components/teamSection";
 
 export function Team() {
     const [data, setData] = useState<TeamData[]>([]); // Estado para armazenar os dados
@@ -23,46 +24,22 @@ export function Team() {
 
     return (
         <div className="flex flex-col w-full h-auto justify-center bg-white dark:bg-zinc-800">
-            <div className="flex flex-col pl-16 py-8 gap-4 bg-white dark:bg-zinc-800">
-                <p className="font-extrabold text-4xl dark:text-white">Principal Investigators</p>
-                <div className="flex flex-row w-full px-6 py-2 gap-8">
-                    {data.length > 0 ? ( // Verifica se há dados para renderizar
-                        data
-                            .filter((teamMember) => teamMember.role.toLowerCase() === "principal") // Filtra para apenas "principal"
-                            .map((teamMember) => (
-                                <div key={teamMember.name} className="h-80 w-60 flex flex-col rounded-2xl bg-sand-100/50 dark:bg-sand-200" >
-                                    <img className="w-full h-3/4 rounded-t-2xl" src={teamMember.picture} alt={teamMember.name} />
-                                    <div className="flex flex-col justify-center align-middle py-4">
-                                        <h3 className="text-lg font-semibold text-center">{teamMember.name}</h3>
-                                        <p className="text-base font-medium text-black/50 text-center">{teamMember.institution}</p>
-                                    </div>
-                                </div>
-                            ))
-                    ) : (
-                        <p className="text-zinc-500">Carregando...</p> // Mensagem de carregamento
-                    )}
-                </div>
-            </div>
-            <div className="flex flex-col pl-16 py-8 gap-4 bg-sand-100/50 dark:bg-sand-200">
-                <p className="font-extrabold text-4xl dark:text-white">Associate Researchers</p>
-                <div className="flex flex-row w-full px-6 py-2 gap-8">
-                    {data.length > 0 ? ( // Verifica se há dados para renderizar
-                        data
-                            .filter((teamMember) => teamMember.role.toLowerCase() === "researchers")
-                            .map((teamMember) => (
-                                <div key={teamMember.name} className="h-80 w-60 flex flex-col rounded-2xl bg-zinc-200 dark:bg-zinc-400" >
-                                    <img className="w-full h-3/4 rounded-t-2xl" src={teamMember.picture} alt={teamMember.name} />
-                                    <div className="flex flex-col justify-center align-middle py-4">
-                                        <h3 className="text-lg font-semibold text-center">{teamMember.name}</h3>
-                                        <p className="text-base font-medium text-black/50 text-center">{teamMember.institution}</p>
-                                    </div>
-                                </div>
-                            ))
-                    ) : (
-                        <p className="text-zinc-500">Carregando...</p> // Mensagem de carregamento
-                    )}
-                </div>
-            </div>
-        </div >
-    );
+          <TeamSection
+            title="Principal Investigators"
+            role="principal"
+            data={data}
+          />
+          <TeamSection
+            title="Associate Researchers"
+            role="researchers"
+            data={data}
+            className="bg-sand-100/50 dark:bg-sand-200"
+          />
+          <TeamSection
+            title="Team Members"
+            role="members"
+            data={data}
+          />
+        </div>
+      );
 }
